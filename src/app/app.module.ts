33f050661844129304e115f7888f6fd236a51e2f
FreddyMartinez <fhmartinezq@gmail.com>
2022-04-02 23:00:33 +0200

    creating principal components & adjusting styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatListComponent } from './components/chat-list/chat-list.component';
+import { ConversationComponent } from './components/conversation/conversation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatListComponent,
+    ConversationComponent
   ],
   imports: [
     BrowserModule,
